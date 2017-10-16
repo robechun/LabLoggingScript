4bbf215e2a2ed6d2f5566c4c8964604960cfeb2f
Robert Chung <robertchung97@gmail.com>
2017-10-16 10:38:54 -0700

    Moved code around to make it more object oriented/modular

--- LabLoggerGeneral.py
+++ LabLoggerGeneral.py
@@ -4,10 +4,12 @@ import time
 spu_username = <YOUR_USERNAME_HERE>
 spu_password = <YOUR_PASSWORD_HERE>
 
-def main():
 
-    # Browser
-    br = mechanize.Browser()
+# Browser
+br = mechanize.Browser()
+
+
+def main():
 
     # Browser options
     br.set_handle_equiv( True ) 
@@ -20,47 +22,14 @@ def main():
     # login/auth
     response = br.open("https://banweb.spu.edu/pls/prod/twbkwbis.P_WWWLogin")    
 
-    # First form window with single sign-on prompt
-    br.select_form(nr=0)
-    br.set_all_readonly( False )
-    br.submit()
-
-    # Login here
-    br.select_form(nr=0)
-    br["username"] = spu_username
-    br["password"] = spu_password
-    br.submit()
-    
-    #print br.response().read()
-
-    # Go to Timesheet selection and select current timesheet
-    req = mechanize.Request("https://banweb.spu.edu/pls/prod/bwpktais.P_SelectTimeSheetRoll")
-    br.open(req)
-    br.select_form(nr=1)
-    br.set_all_readonly( False )
-    br.submit(type="submit")
-
-    
-    # Find Today's time sheet and update
-    todaysURL = getTodaysURL()
-    timesheet = mechanize.Request(todaysURL)
-    br.open(timesheet)
-    br.select_form(nr=1)
-    br.set_all_readonly ( False )
-    
-    timeIN = br.find_control(name="TimeIn", nr=0)
-    timeIN.value = "6:00"
-    timeOUT = br.find_control(name="TS_TimeOut", nr=0)
-    timeOUT.value = "9:30"
-
-    timeIN_AMPM = br.find_control(name="TimeInAm", nr=0)
-    timeIN_AMPM.value = ["PM"]
-    timeOUT_AMPM = br.find_control(name="TimeOutAm", nr=0)
-    timeOUT_AMPM.value = ["PM"]
-
-    br.submit(name="ButtonSelected", label="Save")
+    handleFirstWindow(br)
+    handleLoginToSPU()
+    handleTimesheetSelection()
+    handleUpdateTimesheet()
 
     print("Good shit\n")
+
+
 
 
 # Get today's url, but might need to figure out the LastDate= part
@@ -74,6 +43,48 @@ def getTodaysURL():
     # return "YOUR_URL...&DateSelected=%s-%s-%s&LineNumber=5" % (day, month, year)
 
 
+# Handle First form window with single sign-on prompt
+def handleFirstWindow():
+    br.select_form(nr=0)
+    br.set_all_readonly( False )
+    br.submit()
+
+# Handle logging into actual SPU system
+def handleLoginToSPU():
+    br.select_form(nr=0)
+    br["username"] = spu_username
+    br["password"] = spu_password
+    br.submit()
+    
+# Choose timesheet so that we can go ahead and update/get access to html form that allows update.
+def handleTimesheetSelection():
+    req = mechanize.Request("https://banweb.spu.edu/pls/prod/bwpktais.P_SelectTimeSheetRoll")
+    br.open(req)
+    br.select_form(nr=1)
+    br.set_all_readonly( False )
+    br.submit(type="submit")
+
+# Update your timesheet with times of when one worked.
+def handleUpdateTimesheet():
+    # Find Today's time sheet
+    todaysURL = getTodaysURL()
+    timesheet = mechanize.Request(todaysURL)
+    br.open(timesheet)
+    br.select_form(nr=1)
+    br.set_all_readonly ( False )
+    
+    # Update timesheet
+    timeIN = br.find_control(name="TimeIn", nr=0)
+    timeIN.value = "6:00"
+    timeOUT = br.find_control(name="TS_TimeOut", nr=0)
+    timeOUT.value = "9:30"
+
+    timeIN_AMPM = br.find_control(name="TimeInAm", nr=0)
+    timeIN_AMPM.value = ["PM"]
+    timeOUT_AMPM = br.find_control(name="TimeOutAm", nr=0)
+    timeOUT_AMPM.value = ["PM"]
+
+    br.submit(name="ButtonSelected", label="Save")
 
 if __name__ == "__main__":
     main()
